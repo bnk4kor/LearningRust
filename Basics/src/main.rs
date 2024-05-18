da035579e0d30e530484a64788d15f3ef1eecfe2
bnk4kor <156786891+bnk4kor@users.noreply.github.com>
2024-05-18 05:37:02 +0000

    Add difference

--- Basics/src/main.rs
+++ Basics/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     add();
+    sub();
 }
 
 fn add() {
@@ -7,4 +8,12 @@ fn add() {
     let b = 20;
     let c = a + b;
     println!("Sum of {} and {} is {}", a, b, c);
+}
+
+fn sub() {
+    let a:i32 = 20;
+    let b:i64 = 10;
+    let c:i32 = a-b as i32;
+    println!("Difference of {} and {} is {}", a, b, c);
+    
 }
