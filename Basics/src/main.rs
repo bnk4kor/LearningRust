6f648955efe44f0a64817ab877df258d8148012a
bnk4kor <156786891+bnk4kor@users.noreply.github.com>
2024-05-18 05:33:52 +0000

    First rust program

---
+++ Basics/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    add();
+}
+
+fn add() {
+    let a = 10;
+    let b = 20;
+    let c = a + b;
+    println!("Sum of {} and {} is {}", a, b, c);
+}
---
+++ Basics/target/debug/Basics.d
@@ -0,0 +1,1 @@
+/workspaces/LearningRust/Basics/target/debug/Basics: /workspaces/LearningRust/Basics/src/main.rs
